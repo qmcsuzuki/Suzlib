a420ae5aac242daf4e5d1e131d083c9953d6f8de
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2024-11-11 03:08:46 +0900

    Update Helloworld.test.py

--- python/example/Helloworld.test.py
+++ python/example/Helloworld.test.py
@@ -1,5 +1,5 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
-import example.Helloworld
+import python.example.Helloworld
 
 def main():
     print(example.Helloworld.get_hello_world())
