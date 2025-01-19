5162456854131be28fdbe185d05210474eb48dcf
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-01-20 00:22:28 +0900

    Create RangeAddRangeSum.py

---
+++ python/data_structure/array1D/RangeAddRangeSum.py
@@ -0,0 +1,22 @@
+class RangeAddRangeSum():
+    def __init__(self, n, init=None):
+        if init is not None:
+            init = list(init) + [0]
+        self.bit0 = FenwickTree(n+1,init)
+        self.bit1 = FenwickTree(n+1)
+
+    def range_add(self,l,r,x):
+        self.bit0.add(l,-l*x)
+        self.bit0.add(r,r*x)
+        self.bit1.add(l,x)
+        self.bit1.add(r,-x)
+
+    # 半開区間 [0,r) 上の和
+    def prefix_sum(self,r):
+        return self.bit1.prefix_sum(r)*r + self.bit0.prefix_sum(r)
+
+    def range_sum(self,l,r):
+        return self.prefix_sum(r) - self.prefix_sum(l)
+    
+    def point_get(self,i):
+        return self.range_sum(i,i+1)
