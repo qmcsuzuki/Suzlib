e860a7a60ee4433d7e5be6ee87edd939a3d1be17
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-01-19 22:50:51 +0900

    Update Accumulate2dim.py

--- python/data_structure_2D/Accumulate2dim.py
+++ python/data_structure_2D/Accumulate2dim.py
@@ -1,12 +1,3 @@
-import sys
-readline = sys.stdin.readline
-
-#n = int(readline())
-#*a = map(int,readline().split())
-# ab = [list(map(int,readline().split())) for _ in range()]
-# S = readline().strip()
-# b = [readline().strip() for _ in range()]
-
 class Accumulate2dim:
     """
     a: h*w 行列
@@ -22,12 +13,12 @@ class Accumulate2dim:
                 acc[i*w+j] = a[i-1][j-1] + acc[i*w+j-1] \
                             + acc[i*w+j-w] - acc[i*w+j-w-1]
 
-    def get_sum_from_origin(self,h,w):
+    def prefix_sum(self,h,w):
         #半開長方形 [0,h2)*[0,w2) の和
         assert 0 <= h and 0 <= w
         return self.acc[h*(1+self.w) + w]
 
-    def get_sum(self,h1,h2,w1,w2):
+    def range_sum(self,h1,h2,w1,w2):
         #半開長方形 [h1,h2)*[w1,w2) の和
         assert 0 <= h1 <= h2 and 0 <= w1 <= w2
         return (self.acc[h1*(1+self.w) + w1]
@@ -36,23 +27,23 @@ class Accumulate2dim:
                     + self.acc[h2*(1+self.w) + w2]
                 )
 
-    def get_sum_from_origin_large(self,x,y):
+    def prefix_sum_large(self,x,y):
         # h*w のパターンが無限に繰り返されている場合の半開長方形 [0,x)*[0,y) の和
         assert 0 <= x and 0 <= y
         xq,xr = divmod(x,self.h)
         yq,yr = divmod(y,self.w)
-        return (self.get_sum_from_origin(self.h,self.w)*xq*yq
-            + self.get_sum_from_origin(xr,self.w)*yq
-            + self.get_sum_from_origin(self.h,yr)*xq
-            + self.get_sum_from_origin(xr,yr))
+        return (self.prefix_sum(self.h,self.w)*xq*yq
+            + self.prefix_sum(xr,self.w)*yq
+            + self.prefix_sum(self.h,yr)*xq
+            + self.prefix_sum(xr,yr))
 
-    def get_sum_large(self,h1,h2,w1,w2):
+    def range_sum_large(self,h1,h2,w1,w2):
         # h*w のパターンが無限に繰り返されている場合の半開長方形 [h1,h2)*[w1,w2) の和
         assert 0 <= h1 <= h2 and 0 <= w1 <= w2
-        return (self.get_sum_from_origin_large(h1,w1)
-            - self.get_sum_from_origin_large(h1,w2)
-            - self.get_sum_from_origin_large(h2,w1)
-            + self.get_sum_from_origin_large(h2,w2))
+        return (self.prefix_sum_large(h1,w1)
+            - self.prefix_sum_large(h1,w2)
+            - self.prefix_sum_large(h2,w1)
+            + self.prefix_sum_large(h2,w2))
 
 ##############################################################
 # https://atcoder.jp/contests/abc331/submissions/61751172
@@ -76,15 +67,15 @@ def f(x,y):
 
     xq,xr = divmod(x,n)
     yq,yr = divmod(y,n)
-    ans = seg.get_sum(0,n,0,n)*xq*yq
-    ans += seg.get_sum(0,xr,0,n)*yq
-    ans += seg.get_sum(0,n,0,yr)*xq
-    ans += seg.get_sum(0,xr,0,yr)
+    ans = seg.range_sum(0,n,0,n)*xq*yq
+    ans += seg.range_sum(0,xr,0,n)*yq
+    ans += seg.range_sum(0,n,0,yr)*xq
+    ans += seg.range_sum(0,xr,0,yr)
     
-    res = seg.get_sum_from_origin(n,n)*xq*yq
-    res += seg.get_sum_from_origin(xr,n)*yq
-    res += seg.get_sum_from_origin(n,yr)*xq
-    res += seg.get_sum_from_origin(xr,yr)
+    res = seg.prefix_sum(n,n)*xq*yq
+    res += seg.prefix_sum(xr,n)*yq
+    res += seg.prefix_sum(n,yr)*xq
+    res += seg.prefix_sum(xr,yr)
     
     assert ans == res
     return ans
@@ -92,7 +83,7 @@ def f(x,y):
 
 for _ in range(Q):
     a,b,c,d = map(int,readline().split())
-    ans1 = seg.get_sum_large(a,c+1,b,d+1)
+    ans1 = seg.range_sum_large(a,c+1,b,d+1)
     ans2 = f(c,d) - f(a-1,d) - f(c,b-1) + f(a-1,b-1)
     assert ans1 == ans2
     print(ans1)
