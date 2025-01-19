f32f844481bea79dc9f3fe03aa80faad61e1724a
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-01-19 22:52:08 +0900

    Update and rename array2D_sum.standalone.test.py to 2D_rangesum.standalone.test.py

--- python/data_structure_2D/test/2D_rangesum.standalone.test.py
+++ python/data_structure_2D/test/2D_rangesum.standalone.test.py
@@ -1,11 +1,12 @@
 # competitive-verifier: STANDALONE
 
 from python.data_structure_2D.FenwickTree2D import FenwickTree2D
+from python.data_structure_2D.Accumulate2dim import Accumulate2dim
 
 if __name__ == "__main__":
     from itertools import product
     # test FenwickTree2d
-    for m,n in product(range(1,10),range(1,10)):
+    for n,m in product(range(1,10),range(1,10)):
         init = [[i*j+1 for j in range(m)] for i in range(n)]]
         bit0 = FenwickTree2D(n,m,init)
         bit1 = FenwickTree2D(n,m)
@@ -13,14 +14,15 @@ if __name__ == "__main__":
             bit0.add(i,j,d:=i-j)
             bit1.add(i,j,d+i*j+1)
             init[i][j] += d
+        acc = Accumulate2dim(init)
         
         # check range query
         for r1,r2 in product(range(n+1),range(m+1)):
             Lsum,Rsum = sum(sum(init[x][:r2]) for x in range(r1)), sum(sum(init[x][r2:]) for x in range(r1,n))
-            assert Lsum == bit0.prefix_sum(r1,r2) == bit1.prefix_sum(r1,r2)
-            assert Rsum == bit0.suffix_sum(r1,r2) == bit1.prefix_sum(r1,r2)
+            assert Lsum == bit0.prefix_sum(r1,r2) == bit1.prefix_sum(r1,r2) == acc.prefix_sum(r1,r2)
+            assert Rsum == bit0.suffix_sum(r1,r2) == bit1.suffix_sum(r1,r2)
             
             for l1,l2 in product(range(r1+1),range(r2+1)):
                 rangesum = sum(sum(init[x][l2:r2]) for x in range(l1,r1))
-                assert rangesum == bit0.range_sum(l1,r1,l2,r2) == bit1.range_sum(l1,r1,l2,r2)
+                assert rangesum == bit0.range_sum(l1,r1,l2,r2) == bit1.range_sum(l1,r1,l2,r2) == acc.range_sum(l1,r1,l2,r2)
 
