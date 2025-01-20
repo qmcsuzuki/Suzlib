6c3b0204636ffcd18fe37fb1f4c6757ac632b875
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-01-20 22:09:29 +0900

    Create Unionfind.test.py

---
+++ python/data_structure/unionfind/test/Unionfind.test.py
@@ -0,0 +1,19 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+from python.data_structure.unionfind.Unionfind import Unionfind
+import sys
+readline = sys.stdin.readline
+
+def main():    
+    N,Q = map(int,readline().split())    
+    uf = UnionFind(N)
+    
+    for _ in range(Q):
+        t,u,v = map(int,readline().split())
+        if t == 0:
+            uf.merge(u, v)
+        else:
+            print("1" if uf.issame(u,v) else "0")
+
+if __name__ == '__main__':
+    main()
