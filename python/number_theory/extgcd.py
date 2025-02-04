8a6e78831e1a47be6b4bd0beb6ee40ce738144a1
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-04 20:41:05 +0900

    Create extgcd.py

---
+++ python/number_theory/extgcd.py
@@ -0,0 +1,10 @@
+def extgcd(a,b):
+    x1 = y0 = 0
+    x0 = y1 = 1
+    while b:
+        q = a//b
+        x0,x1 = x1, x0 - q*x1
+        y0,y1 = y1, y0 - q*y1
+        a,b = b, a - q*b
+    return x0, y0, a
+
