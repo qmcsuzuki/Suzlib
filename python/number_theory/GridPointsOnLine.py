868009de862bc43f4e3f0ae0b242595aafdaad62
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-04 20:48:38 +0900

    Update GridPointsOnLine.py

--- python/number_theory/GridPointsOnLine.py
+++ python/number_theory/GridPointsOnLine.py
@@ -1,3 +1,20 @@
+def count_integer_points_of_queen_moves(a,b,c,x_min,x_max,y_min,y_max):
+    # find number of (ax + by = c, x_min <= x < x_max, y_min <= y < y_max)
+    # 範囲が range のように半開区間になっている点に注意
+    # ここで (a,b) は 8 方向を表す （つまり {-1,0,1}^2 から (0,0) を除いたもの）
+    if a == 0:
+        assert b != 0
+        return x_max-x_min if y_min <= c < y_max else 0
+    elif b == 0:
+        return y_max-y_min if x_min <= c//a < x_max else 0
+    if a == -1:
+        a = 1; b = -b; c = -c
+    if b == 1: # y -> -y 置換
+        b = -1; y_min, y_max = -y_max+1, -y_min+1
+    assert a == 1 and b == -1
+    return max(0, min(x_max,c+y_max)-max(x_min,c+y_min))
+
+# https://atcoder.jp/contests/abc377/submissions/62326704
 
 def grid_points_on_line(a,b,c,x_min,x_max,y_min,y_max):
     """
