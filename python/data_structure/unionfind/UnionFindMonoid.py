08d1c3b72d0c9d360ac1cc33fd5a53466826e1ce
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-09 20:24:51 +0900

    Create UnionFindMonoid.py

---
+++ python/data_structure/unionfind/UnionFindMonoid.py
@@ -0,0 +1,24 @@
+class UnionFindMonoid(UnionFind):
+    def __init__(self, n, op, init):
+        super().__init__(n)
+        self.op = op
+        assert len(init)==n
+        self.data = init[::]
+
+    def merge(self, x, y): #merge(x,y): xのいる組とyのいる組をまとめる
+        x, y = self.leader(x), self.leader(y)
+        if x == y: return -1
+        if self.size[x] < self.size[y]: #xの要素数が大きいように
+            x,y=y,x
+        # yを x につなぐ
+        self.size[x] += self.size[y]
+        self.parent[y] = x
+        self.data[x] = self.op(self.data[x], self.data[y])
+        return x
+
+"""
+マージテクの op
+def op(x,y):
+    x += y
+    return x
+"""
