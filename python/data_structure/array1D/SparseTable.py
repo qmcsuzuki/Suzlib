9028ca9084f755ed1a2d467a7de9282368dc5c03
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-16 17:37:42 +0900

    Update SparseTable.py

--- python/data_structure/array1D/SparseTable.py
+++ python/data_structure/array1D/SparseTable.py
@@ -9,7 +9,15 @@ class SparseTable:
             self.table.append(T := [op(T[i],T[i+r]) for i in range(len(T)-r)])
             r *= 2
 
-    #半開区間 [L,R) 上の区間積を返す
+    """
+    /**
+    * @brief 半開区間 [L,R) 上の区間積を返す
+    * @param[in] a(引数名) 引数の説明
+    * @param[out] b(引数名) 引数の説明
+    * @return bool 戻り値の説明
+    * @details 詳細な説明
+    */
+    """
     def prod(self, L,R):
         assert L < R
         i = (R-L).bit_length()-1 #2**i <= R-L < 2**(i+1)
