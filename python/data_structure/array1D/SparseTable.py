e6a2693947109d3f8eab6870f846a286dfc815e4
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-16 17:26:44 +0900

    Create SparseTable.py

---
+++ python/data_structure/array1D/SparseTable.py
@@ -0,0 +1,18 @@
+class SparseTable:
+    def __init__(self, a, op):
+        self.op = op
+        self.table = [a[:]]
+        n = len(a)
+        r = 1 #区間の幅の半分
+        T = self.table[-1]
+        while 2*r <= n:
+            self.table.append(T := [op(T[i],T[i+r]) for i in range(len(T)-r)])
+            r *= 2
+
+    #半開区間 [L,R) 上の区間積を返す
+    def prod(self, L,R):
+        assert L < R
+        i = (R-L).bit_length()-1 #2**i <= R-L < 2**(i+1)
+        return self.op(self.table[i][L], self.table[i][R-(1<<i)])
+
+
