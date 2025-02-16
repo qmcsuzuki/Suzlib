bdf7e50520b03575c258c8d0745a9d08d73200b6
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-17 05:41:37 +0900

    Update sparsetables_standalone.test.py

--- python/data_structure/array1D/test/sparsetables_standalone.test.py
+++ python/data_structure/array1D/test/sparsetables_standalone.test.py
@@ -9,23 +9,25 @@ class SparseTableArgminmax(SparseTable):
         aa = [v*self.M+i for i,v in enumerate(a)]
         super().__init__(aa, min_or_max)
     
-    """
-    値、添え字のペアを返す
-    """
+    # 値、添え字のペアを返す
     def prod(self, L,R):
-        return divmod(super().prod(L,R),self.M)
+        return divmod(super().prod(L,R), self.M)
 
 from random import shuffle
 
 def test_sparse_table(n):
-    # test: 文字列結合 (非可換な場合)
     a = list(range(n))
     shuffle(a)
-    sp = SparseTableArgminmax(a,min)
+    # test 1: argmin
+    # test2: x*y = x (非可換な場合)
+    sp1 = SparseTableArgminmax(a,min)
+    sp2 = SparseTable(a,lambda x,y: x)
     for i in range(n):
         for j in range(i+1,n+1):
-            val,idx = sp.prod(i,j)
+            val,idx = sp1.prod(i,j)
             assert a[idx] == val == min(a[i:j])
+            r = sp2.prod(i,j)
+            assert r == a[i]
 
 def test_disjoint_sparse_table(n):
     # test: 文字列結合 (非可換な場合)
