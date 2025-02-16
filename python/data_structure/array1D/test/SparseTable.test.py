5146b014e157afb54c560ac1e1061e7c35e09c9f
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-02-16 17:29:01 +0900

    Create SparseTable.test.py

---
+++ python/data_structure/array1D/test/SparseTable.test.py
@@ -0,0 +1,21 @@
+# competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/staticrmq
+
+from python.data_structure.array1D.SparseTable import SparseTable
+import sys
+readline = sys.stdin.readline
+
+n,Q = map(int,readline().split())
+*a, = map(int,readline().split())
+
+def argmin(i,j):
+    return i if a[i] <= a[j] else j
+
+sp1 = SparseTable(a,min)
+sp2 = SparseTable(list(range(n)),argmin)
+
+for _ in range(Q):
+    l,r = map(int,readline().split())
+    print(v := sp1.prod(l,r))
+    assert v == a[sp2.prod(l,r)]
+
+
