e222e61041cc9add64c0921b4c0bbeb31d525a30
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-03-09 18:40:14 +0900

    Update PotentialUnionFindGeneral.py

--- python/data_structure/unionfind/PotentialUnionFindGeneral.py
+++ python/data_structure/unionfind/PotentialUnionFindGeneral.py
@@ -1,8 +1,8 @@
 class PotentialUnionFindGeneral:
-    def __init__(self, n, add, inv, e_M):
+    def __init__(self, n, mul, inv, e_M):
         self.parent = [-1]*n #親ノード or size
-        self.potential = [e_M]*n #親ノードを基準としたポテンシャル
-        self.add = add #足し算
+        self.diff_p = [e_M]*n # p(x) = p(parent[x])*diff_p(x) （親ノードを基準としたポテンシャル・根ノードではない）
+        self.mul = mul #積
         self.inv = inv #逆元
         self.e_M = e_M #単位元
 
@@ -11,15 +11,15 @@ class PotentialUnionFindGeneral:
             x = self.parent[x]
         return x 
 
-    def weight(self, x): # p(x) - p(root)
+    def weight(self, x): # p(x) * p(root)^{-1}
         c = self.e_M
         while self.parent[x] >= 0:
-            c = self.add(self.potential[x], c)
+            c = self.mul(self.diff_p[x], c)
             x = self.parent[x]
         return c
 
-    def merge(self, x, y, dxy): #ポテンシャル差p(x)-p(y)=dxyでxとyの組をまとめる
-        dxy = self.add(self.add(self.weight(x),dxy), self.inv(self.weight(y))) #dxyを置き換え
+    def merge(self, x, y, dxy): #ポテンシャル差 p(x)=p(y)*dxy でxとyの組をまとめる
+        dxy = self.mul(self.weight(x), self.inv(self.mul(self.weight(y), dxy))) #dxyを置き換え
         x,y = self.root(x), self.root(y)
         if x == y: return False
         if self.parent[x] > self.parent[y]: #rxの要素数が大きいように
@@ -27,14 +27,15 @@ class PotentialUnionFindGeneral:
             dxy = self.inv(dxy)
         self.parent[x] += self.parent[y] #xの要素数を更新
         self.parent[y] = x #ryをrxにつなぐ
-        self.potential[y] = dxy #ryの相対ポテンシャルを更新
+        self.diff_p[y] = dxy #ryの相対ポテンシャルを更新
         return True
  
     def issame(self, x, y): #same(x,y): xとyが同じ組ならTrue
         return self.root(x) == self.root(y)
         
-    def diff(self,x,y): #diff(x,y): wt(x)-wt(y) を返す 
-        return self.add(self.weight(x), self.inv(self.weight(y)))
+    def diff(self,x,y): #diff(x,y): wt(y)^{-1}*wt(x) を返す 
+        return self.mul(self.inv(self.weight(y)), self.weight(x))
 
     def size(self,x): #size(x): xのいるグループの要素数を返す
         return -self.parent[self.root(x)]
+
