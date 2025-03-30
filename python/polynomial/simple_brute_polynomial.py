f68aa520bc32de592da9dd088b24cc6a9aea1266
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-03-30 16:36:13 +0900

    Update simple_brute_polynomial.py

--- python/polynomial/simple_brute_polynomial.py
+++ python/polynomial/simple_brute_polynomial.py
@@ -5,6 +5,13 @@ def polyadd(f,g):
         res[i] = (res[i]+v)%MOD
     return res
 
+def polysub(f,g):
+    L = max(len(f),len(g))
+    res = f[::] + [0]*(L-len(f))
+    for i,v in enumerate(g):
+        res[i] = (res[i]-v)%MOD
+    return res
+
 def polymul(f,g,maxdegree=None):
     L = len(f)+len(g)-1
     if maxdegree is not None:
