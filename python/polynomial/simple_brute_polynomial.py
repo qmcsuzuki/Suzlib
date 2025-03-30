ccceec239467860e2819664aa6b449dbe605fadc
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-03-30 16:34:11 +0900

    Create simple_brute_polynomial.py

---
+++ python/polynomial/simple_brute_polynomial.py
@@ -0,0 +1,17 @@
+def polyadd(f,g):
+    L = max(len(f),len(g))
+    res = f[::] + [0]*(L-len(f))
+    for i,v in enumerate(g):
+        res[i] = (res[i]+v)%MOD
+    return res
+
+def polymul(f,g,maxdegree=None):
+    L = len(f)+len(g)-1
+    if maxdegree is not None:
+        L = min(L, maxdegree+1)
+    res = [0]*L
+    for i,v in enumerate(f):
+        for j in range(L-i):
+            res[i+j] = (res[i+j] + v*g[j])%MOD
+    return res
+  
