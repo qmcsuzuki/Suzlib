adf2408d9c5f1ca1adcb342352d523cce3e73fe2
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-04-07 02:19:49 +0900

    Update bipartite.py

--- python/graph/bipartite.py
+++ python/graph/bipartite.py
@@ -6,19 +6,23 @@ class bipartite(UnionFind):
         self.n = n
         self.is_bipartite = True
 
-    def add_edge(self,u,v):
+    def add_edge(self,u,v): # 頂点 u,v を辺で結ぶ（違う色という情報を与える）
         self.merge(u,v+self.n)
         self.merge(u+self.n,v)
         if self.issame(u,u+self.n):
             self.is_bipartite = False
 
-    def add_same(self,u,v):
+    def add_same(self,u,v): # 頂点 u,v が同じ色という情報を与える
         self.merge(u,v)
         self.merge(u+self.n,v+self.n)
         if self.issame(u,u+self.n):
             self.is_bipartite = False
 
     def all_connected_components(self):
+        """
+        全ての連結成分についての （白頂点の個数、黒頂点の個数）のペアのリストを返す
+        連結成分が違えば白黒は入れ替えられることに注意
+        """
         res = []
         used = [0]*self.n
         for lst in self.groups():
@@ -35,5 +39,5 @@ class bipartite(UnionFind):
             res.append((x,y))
         return res
 
-    def number_of_connected_component(self):
+    def number_of_connected_component(self): # 現在の連結成分数を返す O(1)
         return self.num_conn_comp//2
