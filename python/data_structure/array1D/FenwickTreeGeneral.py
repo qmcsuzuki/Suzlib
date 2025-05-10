0cc151ecc16ec0a8de7ef587881f12043a0c2d8d
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-05-10 23:46:09 +0900

    Create FenwickTreeGeneral.py

---
+++ python/data_structure/array1D/FenwickTreeGeneral.py
@@ -0,0 +1,50 @@
+"""
+一般の演算 op に関する Fenwick tree
+あまり検証していない
+
+- op は可換が必要
+- prefix_sum 以外を使う場合逆演算 inv が必要
+"""
+
+class FenwickTreeGeneral:
+    def __init__(self, n, op, e, inv, init=None):
+        self.size = n
+        self.longest_interval = 1<<(n.bit_length()-1)
+        self.e = e
+        self.op = op
+        self.inv = inv
+        if init is None:
+            self.data = [e]*n
+        else:
+            assert 0
+            """
+            self.data = list(init)
+            assert len(self.data) == n
+            for i in range(n):
+                i_above = i + ((i+1) & -(i+1))
+                if i_above < n: self.data[i_above] += self.data[i]
+            """
+
+    def prefix_sum(self, r):
+        """ 半閉区間 [0,r) 上の和 a[0] + ... + a[r-1] を返す """
+        s = self.e
+        while r > 0:
+            s = self.op(s,self.data[r-1])
+            r -= r & -r
+        return s
+
+    def range_sum(self,l,r):
+        """ 半閉区間 [l,r) 上の和 a[l] + ... + a[r-1] を返す """
+        return self.op(self.prefix_sum(r), self.inv(self.prefix_sum(l)))
+
+    def suffix_sum(self,l): #a_l + ... (端まで)
+        """ l 以上の添字での和 a[l] + a[l+1] + ... + を返す """
+        return self.op(self.prefix_sum(self.size), self.inv(self.prefix_sum(l)))
+
+    def add(self, i, x):
+        """ a[i] += x """
+        i += 1
+        while i <= self.size:
+            self.data[i-1] = self.op(self.data[i-1],x)
+            i += i & -i
+
