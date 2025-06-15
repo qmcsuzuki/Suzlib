b621d3c0a37b37eb866944b190bb3fd2f00b18d6
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-06-15 16:23:15 +0900

    Create XorBasis.py

---
+++ python/misc/XorBasis.py
@@ -0,0 +1,19 @@
+"""
+xor 基底を管理する
+"""
+class XorBasis:
+    def __init__(self):
+        self.basis = []
+
+    def add_basis(self, x): # 基底集合に x を追加
+        x = self.normalize(x)
+        if x: self.basis.append(x)
+
+    def normalize(self, x): # ベクトル x を基底で掃き出して標準化する
+        for v in self.basis:
+            if v^x < x:
+                x ^= v
+        return x
+    
+    def get_sorted_basis(self): # 降順ソートした基底を得る（貪欲法などに使用）
+        return sorted(self.basis, reverse=1)
