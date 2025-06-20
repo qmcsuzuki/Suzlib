d53b55552bf65b699b9df70804f7e3ab7b8c16ae
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-06-20 19:15:43 +0900

    Update DAGof2dPoints.py

--- python/misc/DAGof2dPoints.py
+++ python/misc/DAGof2dPoints.py
@@ -1,5 +1,5 @@
-# https://atcoder.jp/contests/arc200/submissions/66899857
-
+# https://atcoder.jp/contests/arc165/submissions/66900861
+# https://atcoder.jp/contests/arc200/submissions/66900888
 """
 2 次元平面上の点について、(x[i] <= x[j] かつ y[i] <= y[j] なら辺 ij を張る)というDAG を考える
 この関数は、補助頂点を使って O(N log N) 本の辺からなる DAG を作る
@@ -7,30 +7,33 @@
 """
 def DAGof2dPoints(points, x_reverse, y_reverse):
     def solveDC(l,r):
-        # return list of index + (flag of is_R), sorted by key=lambda i:Y[i], reverse = y_reverse
+        # return list of index , sorted by key=lambda i:Y[i], reverse = y_reverse
         nonlocal g
         if l+1 == r:
-            i = X[l]&((1<<20)-1)
-            return [Y[i]*2]
+            return [Y[X[l]&((1<<20)-1)]]
 
         m = (l+r)//2
-        Ys = [i&(~1) for i in solveDC(l,m)] + [i|1 for i in solveDC(m,r)]
-        Ys.sort(reverse=y_reverse)
-        
+        Y2 = solveDC(m,r)
         offset = len(g)
-        for i in range(len(Ys)-1):
-            g.append([offset+i+1])
-        g.append([])
-        
-        for i,y_info in enumerate(Ys):
-            is_R = y_info&1
-            idx = (y_info>>1)&((1<<20)-1)
-            if is_R:
-                g[offset+i].append(idx)
-            else:
-                g[idx].append(offset+i)
-        return Ys
+        len_y2 = len(Y2)
+        for i in range(len_y2-1):
+            g.append([offset+i+1, Y2[i]&((1<<20)-1)])
+        g.append([Y2[-1]&((1<<20)-1)])
 
+        Y1 = solveDC(l,m)
+        L = 0
+        for y_info in Y1:
+            y1,idx = divmod(y_info,1<<20)
+            while L < len_y2 and ((not y_reverse and y1 > Y2[L]>>20) or
+                                  (y_reverse and y1 < Y2[L]>>20)):
+                L += 1
+            if L == len_y2: break
+            g[idx].append(offset+L)
+
+        Y1 += Y2
+        Y1.sort(reverse=y_reverse)
+        return Y1
+    
     n = len(points)
     assert n < (1<<20)
     X = [0]*n
