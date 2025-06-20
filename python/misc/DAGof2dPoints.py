dda30c3709cf0986795aef787b6bf28605580aae
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-06-20 18:15:32 +0900

    Update DAGof2dPoints.py

--- python/misc/DAGof2dPoints.py
+++ python/misc/DAGof2dPoints.py
@@ -1,5 +1,10 @@
 # https://atcoder.jp/contests/arc200/submissions/66899857
 
+"""
+2 次元平面上の点について、(x[i] <= x[j] かつ y[i] <= y[j] なら辺 ij を張る)というDAG を考える
+この関数は、補助頂点を使って O(N log N) 本の辺からなる DAG を作る
+（注: 不等号の向きは x_reverse, y_reverse で指定可能）
+"""
 def DAGof2dPoints(points, x_reverse, y_reverse):
     def solveDC(l,r):
         # return list of index + (flag of is_R), sorted by key=lambda i:Y[i], reverse = y_reverse
