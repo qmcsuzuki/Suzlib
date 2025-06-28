ed17a80088ccf220f996a79c948be179f861402a
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-06-29 03:46:37 +0900

    Create RangeSieve.py

---
+++ python/math/prime/RangeSieve.py
@@ -0,0 +1,15 @@
+"""
+区間篩: R-L および \sqrt(R) が十分小さいとき、
+L 以上 R 以下の数が素数かどうかを offset 付きの配列で返す
+"""
+def RangeSieve(L,R):
+    assert 0 <= L <= R
+    Eratosthenes_prime_lst = Eratosthenes(int(R**0.5)+1)
+    is_prime = [1]*(R-L+1)
+    for v in range(2): # 0,1 は素数でない
+        if L <= v <= R:
+            is_prime[L-v] = 0
+    for p in Eratosthenes_prime_lst:
+        for i in range((L+p-1)//p*p,R+1,p):
+            if i != p: is_prime[i-L] = 0
+    return Eratosthenes_prime_lst, is_prime
