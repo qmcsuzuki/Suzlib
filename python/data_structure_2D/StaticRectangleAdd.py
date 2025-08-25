bcc3a99994a31b3722bcf9dc8f80e7382a844fbd
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-08-25 22:41:17 +0900

    Create StaticRectangleAdd.py

---
+++ python/data_structure_2D/StaticRectangleAdd.py
@@ -0,0 +1,28 @@
+class StaticRectangleAdd:
+    """
+    H*W の長方形加算
+    最後に和を計算する
+    """
+    def __init__(self,H,W):
+        self.H = H
+        self.W = W
+        self.data = [[0]*(W+1) for _ in range(H+1)]
+
+    def add_rectangle(self,h1,h2,w1,w2,val):
+        """半開長方形 [h1:h2]*[w1:w2] 上に val を加える"""
+        self.data[h1][w1] += val
+        self.data[h1][w2] -= val
+        self.data[h2][w1] -= val
+        self.data[h2][w2] += val
+    
+    def solve(self):
+        """各点の値を求める（１回しかできない）"""
+        for lst in self.data:
+            for j in range(self.W):
+                lst[j+1] += lst[j]
+        
+        for i in range(self.H):
+            for j in range(self.W):
+                self.data[i+1][j] += self.data[i][j]
+        
+        return [self.data[i][:-1] for i in range(self.H)]
