2ee5b8ee48795e1cb92be749cdcc00ff1c88b507
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-11-02 02:15:54 +0900

    Implement OrderedMultisetWithZaatu class

---
+++ python/data_structure/array1D/OrderedMultisetZaatu.py
@@ -0,0 +1,42 @@
+from bisect import bisect_left, bisect_right
+class OrderedMultisetWithZaatu:
+    def __init__(self, values):
+        self.sortedvalues = sorted(set(values))
+        self.za = {v:i for i,v in enumerate(self.sortedvalues)}
+        self.bit = FenwickTree(len(self.sortedvalues)) #存在すれば 1、しないなら 0
+
+    def __contains__(self,v):
+        idx = self.za[v]
+        return self.bit.range_sum(idx,idx+1) > 0
+
+    def add(self,v,wt=1): # 値 v を重み wt で追加（通常重み=1）
+        self.bit.add(self.za[v],wt)
+        
+    def delete(self,v): # 値を削除
+        self.add(v,-1)
+
+    def kth_index(self,k): # k 番目 (1-indexed) に小さい元の index を求める
+        return self.bit.bisect_left(k)
+
+    def kth_value(self,k): # k 番目に小さい元の値を求める
+        return self.sortedvalues[self.kth_index(k)]
+
+    def prev_key(self,v): #一個前の元のkeyを求める
+        idx = bisect_left(self.sortedvalues,v)
+        s = self.bit.prefix_sum(idx)
+        assert s != 0
+        idx = self.bit.bisect_left(s)
+        return idx
+
+    def prev_value(self,v):
+        return self.sortedvalues[self.prev_key(v)]
+
+    def next_key(self,v): #次の元のkeyを求める
+        idx = bisect_right(self.sortedvalues,v)
+        s = self.bit.prefix_sum(idx)
+        idx = self.bit.bisect_left(s+1)
+        assert idx != self.bit.size
+        return idx
+
+    def next_value(self,v):
+        return self.sortedvalues[self.next_key(v)]
