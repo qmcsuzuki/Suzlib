f027064c5e064a405ade70592a0fcc5f0e5d022d
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-11-24 16:56:58 +0900

    Implement StaticRangeSum class for range queries

---
+++ python/data_structure/array1D/StaticRangeSum.py
@@ -0,0 +1,11 @@
+class StaticRangeSum:
+    def __init__(self, init):
+        acc = self.acc = list(init)
+        acc.append(0)
+        for i in range(len(acc)-2)[::-1]: # 右から累積和
+            acc[i] += acc[i+1]
+
+    def range_sum(self, l, r):
+        """ 半閉区間 [l,r) 上の和 a[l] + ... + a[r-1] を返す """
+        assert l <= r
+        return self.acc[l] - self.acc[r]
