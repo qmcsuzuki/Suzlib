a052369391454610ccac73bda05c37fffc0ff94e
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-11-30 23:30:16 +0900

    Create bfs.py

---
+++ python/graph/bfs.py
@@ -0,0 +1,15 @@
+def bfs(g,start):
+    n = len(g)
+    #prev = [-1]*n
+    bfs_order = [start]
+    dist = [-1]*n
+    dist[start] = idx = 0
+    while idx < len(bfs_order):
+        u = bfs_order[idx]
+        for v in g[u]:
+            if dist[v] != -1: continue
+            dist[v] = dist[u] + 1
+            #prev[v] = u
+            bfs_order.append(v)
+        idx += 1
+    return bfs_order, dist #,prev
