10d7e78f4cd4aa4e24711ab69a1148211e9b8495
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-12-08 03:41:39 +0900

    Create OrderedMultiset.test.py

---
+++ python/data_structure/array1D/test/OrderedMultiset.test.py
@@ -0,0 +1,36 @@
+# competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/predecessor_problem
+
+from python.data_structure.array1D.FenwickTree import FenwickTree
+from python.data_structure.array1D.OrderedMultiset import OrderedMultiset
+import sys
+readline = sys.stdin.readline
+
+def main():
+    n,Q = map(int,readline().split())
+    T = readline().strip()
+    
+    m,M = -1,n+1
+    S = OrderedMultiset(n,M)
+    for i in range(n):
+        if T[i] == "1":
+            S.add(i)
+    
+    for _ in range(Q):
+        c,k = map(int,readline().split())
+        if c == 0:
+            if k not in S:
+                S.add(k)
+        elif c == 1:
+            if k in S:
+                S.delete(k)
+        elif c == 2:
+            print(int(k in S))
+        elif c == 3:
+            r = S.next_value(k-1)
+            print(r if r != M else -1)
+        elif c == 4:
+            r = S.prev_value(k+1)
+            print(r if r != m else -1)
+
+if __name__ == '__main__':
+    main()
