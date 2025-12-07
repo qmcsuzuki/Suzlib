20547d5d8ae5f7993b941a3c9bb08642cc39459f
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-12-08 02:08:39 +0900

    Update OrderedMultisetZaatu.py

--- python/data_structure/array1D/OrderedMultisetZaatu.py
+++ python/data_structure/array1D/OrderedMultisetZaatu.py
@@ -1,17 +1,24 @@
 """
-座標圧縮+ BIT の ordered set もどき
-番兵は自分で挿入すること
+座標圧縮+ BIT の ordered multiset もどき
+番兵は、勝手に集合に挿入される
 引数: 
 - values: 集合に入りうる値をクエリ先読み
-提出例: https://atcoder.jp/contests/abc430/submissions/70631379
+- banhei_min: 番兵（全ての values より小さい値）
+- banhei_max: 番兵（全ての values より大きい値）
+提出例:
+- https://atcoder.jp/contests/abc430/submissions/70631379
+- https://judge.yosupo.jp/submission/335160
 """
 
 from bisect import bisect_left, bisect_right
 class OrderedMultisetWithZaatu:
-    def __init__(self, values):
-        self.sortedvalues = sorted(set(values))
+    def __init__(self, values, banhei_min, banhei_max):
+        assert banhei_min < min(values) and max(values) < banhei_max
+        self.sortedvalues = [banhei_min] + sorted(set(values)) + [banhei_max]
         self.za = {v:i for i,v in enumerate(self.sortedvalues)}
         self.bit = FenwickTree(len(self.sortedvalues)) #存在すれば 1、しないなら 0
+        self.add(banhei_min)
+        self.add(banhei_max)
 
     def __contains__(self,v):
         idx = self.za[v]
@@ -37,15 +44,15 @@ class OrderedMultisetWithZaatu:
         idx = self.bit.bisect_left(s)
         return idx
 
-    def prev_value(self,v):
+    def prev_value(self,v): # v の一個前の元の値（なければ、番兵（小））
         return self.sortedvalues[self.prev_index(v)]
 
-    def next_index(self,v): #次の元の sortedvalues における index
+    def next_index(self,v): #一個次の元の sortedvalues における index
         idx = bisect_right(self.sortedvalues,v)
         s = self.bit.prefix_sum(idx)
         idx = self.bit.bisect_left(s+1)
         assert idx != self.bit.size
         return idx
 
-    def next_value(self,v):
+    def next_value(self,v):# v の一個次の元の値（なければ、番兵（大））
         return self.sortedvalues[self.next_index(v)]
