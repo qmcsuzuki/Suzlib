9fe781e473318d68f5f6e3933a68ca89b62283e6
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-12-10 17:13:50 +0900

    Fix variable name and update print statement in tests

--- python/data_structure/array1D/test/CartesianTree.test.py
+++ python/data_structure/array1D/test/CartesianTree.test.py
@@ -8,11 +8,14 @@ readline = sys.stdin.readline
 n = int(readline())
 *a, = map(int, readline().split())
 
-parent = P1 = Cartesian_tree_simple(a)
+par = P1 = Cartesian_tree_simple(a)
 L,R,P2 = Cartesian_tree_full(a)
 P3 = [0]*n
 def calc(i,l,r,p): P3[i] = p
 Cartesian_tree_DFSsearch(a,calc)
 
 assert P1 == P2 == P3
-print(*[p if p != -1 else i for i, p in enumerate(parent)])
+
+r = par.index(-1)
+par[r] = r
+print(*par)
