2b0c7926b96485c867942120d2b1b782184225f3
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-12-15 01:26:29 +0900

    Create SWAG.test.py

---
+++ python/data_structure/array1D/test/SWAG.test.py
@@ -0,0 +1,23 @@
+# competitive-verifier: https://judge.yosupo.jp/problem/queue_operate_all_composite
+
+from python.data_structure.array1D.SlidingWindowAggregation import SWAG
+
+import sys
+readline = sys.stdin.readline
+
+def merge(a,b):
+    return (a[0]*b[0]%MOD,(b[0]*a[1]+b[1])%MOD)
+swag = SWAG(merge,(1,0))
+
+MOD = 998244353
+q = int(input())
+for _ in range(q):
+    a = [int(i) for i in input().split()]
+    if a[0]==0:
+        swag.append((a[1],a[2]))
+    elif a[0]==1:
+        swag.popleft()
+    else:
+        p,q = swag.fold_all()
+        print((p*a[1]+q)%MOD)
+
