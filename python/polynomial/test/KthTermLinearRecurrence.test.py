4bfff45e5cc02511b14b08ce94f3fe89a3c6f6ab
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-01-17 18:13:27 +0900

    Update KthTermLinearRecurrence.test.py

--- python/polynomial/test/KthTermLinearRecurrence.test.py
+++ python/polynomial/test/KthTermLinearRecurrence.test.py
@@ -1,9 +1,10 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence
 import python.polynomial.LinearRecurrence as LinearRecurrence
+import python.polynomial.simple_brute_polynomial as simple_brute_polynomial
 
 MOD = 998244353
 LinearRecurrence.MOD = MOD
-
+simple_brute_polynomial.MOD = MOD
 
 def main():
     k, n = map(int, input().split())
