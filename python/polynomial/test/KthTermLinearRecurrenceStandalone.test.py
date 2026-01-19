ed5c347be16a2ec700e73d7ab64108146acf4aa3
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-01-19 14:08:33 +0900

    Update KthTermLinearRecurrenceStandalone.test.py

--- python/polynomial/test/KthTermLinearRecurrenceStandalone.test.py
+++ python/polynomial/test/KthTermLinearRecurrenceStandalone.test.py
@@ -29,8 +29,8 @@ def check_case(initial, coeffs):
 
 def main():
     cases = [
-        ([4, -5, 6, -7], [-1, 0, -2, 3]),
-        ([1, 3], [0, 2]),
+        ([4, 5, 6, 7], [-1, 0, -2, 3]),
+        ([1, 3], [0, -2]),
         ([2, 0, 5], [0, 0, 1]),
     ]
     for initial, coeffs in cases:
