11106b24358fa8c6ec872e2b64d98211364a07ab
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-01-19 13:56:06 +0900

    Comment out main function and imports in test file
    Comment out the main function and related imports for testing.

--- python/polynomial/test/KthTermLinearRecurrence.test.py
+++ python/polynomial/test/KthTermLinearRecurrence.test.py
@@ -1,19 +1,25 @@
+# verification-helper: STANDALONE
+#
+# need fast polynomial multiplication
+
+return
+
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence
-import python.polynomial.LinearRecurrence as LinearRecurrence
-import python.polynomial.simple_brute_polynomial as simple_brute_polynomial
+# import python.polynomial.LinearRecurrence as LinearRecurrence
+# import python.polynomial.simple_brute_polynomial as simple_brute_polynomial
 
-MOD = 998244353
-LinearRecurrence.MOD = MOD
-simple_brute_polynomial.MOD = MOD
+# MOD = 998244353
+# LinearRecurrence.MOD = MOD
+# simple_brute_polynomial.MOD = MOD
 
-def main():
-    k, n = map(int, input().split())
-    a = list(map(int, input().split()))
-    c = list(map(int, input().split()))
-    g = [1] + [(-ci) % MOD for ci in c]
-    ans = LinearRecurrence.rec_nth_term(a, g, n)
-    print(ans)
+# def main():
+#     k, n = map(int, input().split())
+#     a = list(map(int, input().split()))
+#     c = list(map(int, input().split()))
+#     g = [1] + [(-ci) % MOD for ci in c]
+#     ans = LinearRecurrence.rec_nth_term(a, g, n)
+#     print(ans)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
