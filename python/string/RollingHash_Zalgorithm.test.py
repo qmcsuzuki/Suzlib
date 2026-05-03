c67c86d9b0c6d43fb4ce63bdd1809414a59f9713
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-05-03 17:33:19 +0900

    Add yosupo zalgorithm verifier for RollingHash61

---
+++ python/string/RollingHash_Zalgorithm.test.py
@@ -0,0 +1,18 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/zalgorithm
+import sys
+from python.string.RollingHash import RollingHash61
+
+readline = sys.stdin.readline
+
+
+def main():
+    s = readline().strip()
+    n = len(s)
+
+    rh = RollingHash61(s)
+    ans = [rh.common_prefix_length(0, n, rh, i, n) for i in range(n)]
+    print(*ans)
+
+
+if __name__ == "__main__":
+    main()
