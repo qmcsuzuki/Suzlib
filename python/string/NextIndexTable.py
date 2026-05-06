b2cdbd53bef221ea949dc267e5a89f7c947a20b0
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-05-06 18:26:09 +0900

    Create NextIndexTable.py

---
+++ python/string/NextIndexTable.py
@@ -0,0 +1,14 @@
+"""
+s を 1-indexed で見る
+nxt[i][j] = i+1 文字目以降に文字種 j の最初のindex
+"""
+def next_index(s):
+    N = len(s)
+    D = [-1]*26
+    nxt = [None]*(N+1)
+    a = ord('a')
+    for i in range(1,N+1)[::-1]:
+        nxt[i] = D[:]
+        D[ord(s[i-1])-a] = i
+    nxt[0] = D
+    return nxt
