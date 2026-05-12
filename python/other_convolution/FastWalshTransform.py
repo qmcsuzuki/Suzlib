09224d301a6fd410e361e9b7faf8c2229621e287
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-05-13 04:00:10 +0900

    Update FastWalshTransform.py

--- python/other_convolution/FastWalshTransform.py
+++ python/other_convolution/FastWalshTransform.py
@@ -1,25 +1,22 @@
 """
 in place な高速Walsh変換
 xor畳み込みなどに用いる。
-配列 a の長さは2ベキ
-M = max(a) のとき、max(fwt(a)) <= len(a)*M なので、
-中でmodをとらなくても、オーバーフローはだいたいは大丈夫なはず
-
-ifwt では、最後にまとめて len(a) で割り算していることに注意
-H^2 = len(a)I なので。
 """
 
 def fwt_inplace(a):
     n = len(a)
+    assert n&(n-1) == 0 # 長さは 2 ベキ
     i = 1
     while i < n:
         for j in range(n):
             if i&j==0:
                 a[j], a[j|i] = a[j]+a[j|i], a[j]-a[j|i]
         i <<= 1
+    for i in range(n):
+        a[i] %= MOD
         
 def ifwt_inplace(a):
     fwt(a)
     v = pow(len(a),MOD-2,MOD)
     for i in range(len(a)):
-      a[i] = v*(a[i]%MOD)%MOD
+      a[i] = v*a[i]%MOD
