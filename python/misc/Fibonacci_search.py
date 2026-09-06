07e09200fa98b6924a3294d43ab01feb7afd3f05
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-09-06 23:09:52 +0900

    Fix Fibonacci search half-open interval

--- python/misc/Fibonacci_search.py
+++ python/misc/Fibonacci_search.py
@@ -6,18 +6,18 @@
 def Fibonacci_search(l,r,f):
     L = l-1
     a,b = 1,2
-    while L+b <= r:
+    while L+b < r:
         a,b = b,a+b
     A,R = L+a, L+b
     fA = f(A)
     # 以下 (L,A,R) を管理
     while R-L > 2:
         B = L+R-A
-        if B <= r:
+        if B < r:
             fB = f(B)
             if A > B:
                 fA,fB,A,B = fB,fA,B,A
-        if B <= r and fA > fB:
+        if B < r and fA > fB:
             L,A = A,B
             fA = fB
         else:
