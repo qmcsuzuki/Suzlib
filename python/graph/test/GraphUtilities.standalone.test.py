7c775a5f7a8eacba78f7ce04fc917fb9fe704d63
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2026-09-14 22:44:59 +0900

    Remove ineffective graph utility checks

--- python/graph/test/GraphUtilities.standalone.test.py
+++ python/graph/test/GraphUtilities.standalone.test.py
@@ -3,9 +3,6 @@
 from random import Random
 
 from python.graph.minimum_distance.FloydWarshall import FloydWarshall
-from python.graph.tree.RootedTreeHash import rooted_tree_hash
-from python.graph.tree.TreeBasic import get_order_and_parent
-from python.graph.grid.LargeGridUF import LargeGridUF
 
 
 def brute_dist(n, edges):
@@ -43,18 +40,3 @@ if __name__ == "__main__":
                 else:
                     f.construct_edge_directed(u,v,w)
                 assert f.D == brute_dist(n,edges)
-    assert rooted_tree_hash([[1],[0]],0)[0] != rooted_tree_hash([[1],[0]],0)[1]
-    for n in range(1,30):
-        g = [[] for _ in range(n)]
-        for v in range(1,n):
-            p = rng.randrange(v)
-            g[p].append(v)
-            g[v].append(p)
-        root = rng.randrange(n)
-        order, par = get_order_and_parent(g,root)
-        actual = rooted_tree_hash(g,root)
-        expected = rooted_tree_hash(g,root,par,order)
-        assert all((actual[i] == actual[j]) == (expected[i] == expected[j])
-                   for i in range(n) for j in range(n))
-    uf, blocks = LargeGridUF(2,3,[])
-    assert uf.issame(blocks[0][0] % (1 << 20), blocks[1][0] % (1 << 20))
